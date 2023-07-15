892f74426f7613536a05d0e040a2d20b5e1fd0d6
NzubeJoy <natenzzy.cakes@gmail.com>
2023-07-15 02:10:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints single digits of base ten
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int f;
+
+	for (f = 0; f < 10; f++)
+		printf("%d", f);
+	putchar('\n');
+	return (0);
+}
