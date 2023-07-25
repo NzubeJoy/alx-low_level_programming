9e7a1854c742e90573fd033ef86d680029655168
NzubeJoy <natenzzy.cakes@gmail.com>
2023-07-25 05:22:58 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: input string
+ */
+
+void puts_half(char *str)
+{
+	int full string, half string;
+
+	full string = 0;
+
+	while (str[full string] != '\0')
+		full string++;
+
+	half string = full string / 2;
+
+	if (full string % 2 == 1)
+		half string++;
+	while (half string < full string)
+	{
+	_putchar(str[half_string];
+		half string++;
+		}
+	_putchar('\n');
+	}
